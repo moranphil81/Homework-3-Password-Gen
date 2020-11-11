b2bc563c4b729e6332165353c047cdc4ba1fb524
moranphil81 <71897509+moranphil81@users.noreply.github.com>
2020-11-10 21:21:37 -0500

    created generate  password function

--- script.js
+++ script.js
@@ -1,6 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+console.log("generatePaswword")
+var randomPassword;
+
+//logic reassgn random password
+
+
+//get user input
+
+
+//get length from user
+var length = prompt("How long do you want your password 8-128")
+
+//check length to make sure it is between 8-128
+if(length <8) {
+  alert("make sure password is at least 8 characters")
+return ""
+}
+if(length >128){
+alert("make sure password doesnt exceed 128 characters")
+} 
+
+//get character types
+
+// Get character types (numbers, uppercase,lowercase,special characters)
+
+
+// check to see at least one character type has been selected then 
+
+// Generate password
+
+//
+
+//
+
+//
+randomPassword="replace this with random password"
+
+
+
+
+
+
+
+return randomPassword
+}
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
